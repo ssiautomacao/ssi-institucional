e3379b2aeb0ae22951eb0cb91e4e65f33942f15a
ssiautomacao <andre@ssi-ind.com>
2026-01-28 22:43:54 -0300

    Lançamento oficial SSI Astro - v1.0

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://beta.ssi-ind.com',
+  base: '/',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()]
+});
